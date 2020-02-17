dc7d69c213927916ea33f2afa6782e961c724084
LiorBaraban <barabanlior@gmail.com>
2020-02-17 10:51:54 +0200

    changed index.js to include .env loading and db authentication

--- apps-server/employees/index.js
+++ apps-server/employees/index.js
@@ -1,1 +1,11 @@
-console.log('TODO - BUILD EMPLOYEES MICROSERVICE');
+require('./startup/config');
+const db = require('./startup/db');
+
+
+db.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
