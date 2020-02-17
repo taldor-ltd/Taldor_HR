a5cca39a17cd5ec784a23fcb5ba9dbeac3973868
LiorBaraban <barabanlior@gmail.com>
2020-02-17 17:33:47 +0200

    given examples for querying data with sequelize

--- apps-server/employees/index.js
+++ apps-server/employees/index.js
@@ -1,11 +1,35 @@
+/* eslint-disable prefer-template */
+const { DataTypes } = require('sequelize');
 require('./startup/config');
 const db = require('./startup/db');
 
 
-db.authenticate()
+db.sequelize.authenticate()
   .then(() => {
     console.log('Connection has been established successfully.');
   })
   .catch((err) => {
     console.error('Unable to connect to the database:', err);
   });
+
+
+// db.employee.findAll({
+//   include: {
+//     model: db.skill,
+//   }
+// }).then((e) => console.log('\n' + JSON.stringify(e)));
+
+
+// db.employee.findAll({
+//   include: {
+//     model: db.user
+//   }
+// }).then((e) => console.log('\n' + JSON.stringify(e)));
+
+// db.project.findAll({
+//   include: {
+//     model: db.skill
+//   }
+// }).then((e) => console.log('\n' + JSON.stringify(e)));
+
+db.function('fn_user_get_login_details', ['admin']).then(([results, meta]) => console.log('\n' + JSON.stringify(results)));
