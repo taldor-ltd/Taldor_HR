5e71cad1cfbe496a10e8a2fa09a4f43e817d4daf
Hen Hadayo <Henh@Taldor.corp>
2020-02-27 15:25:32 +0200

    implmenetd login api

---
+++ apps-server/api-gateway/routes/authenticates.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+
+router.route("/login").post((req, res) => {
+    // const user = {
+    //     username: req.body.username,
+    //     password: req.body.password
+    // };
+    //const { username, password } = req.body;
+    res.status(200).json(req.body);
+});
+
+module.exports = router;
