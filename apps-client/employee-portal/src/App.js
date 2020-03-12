6d90181af259eafa26988294ded4f6a22ffc0d74
LiorBaraban <oracleilb@gmail.com>
2020-03-12 13:11:54 +0200

    Revert "Login api db"

--- apps-client/employee-portal/src/App.js
+++ apps-client/employee-portal/src/App.js
@@ -1,44 +1,26 @@
 import React from 'react';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import "./assets/css/styles.css";
-import "./assets/css/inspinia.css";
-import "./assets/css/inspinia-tuning.css";
-import   "./assets/css/animate.css";
-import 'primereact/resources/themes/nova-light/theme.css';
-import 'primereact/resources/themes/nova-light/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import {
-  BrowserRouter as Router, Route, Link,
-  Switch,
-  Redirect
-} from "react-router-dom";
-import Login from "./components/_login/Login";
-import Dashboard from "./components/_dashboard/Dashboard";
-import ProfileContact from "./components/_profileContact/ProfileContact"
-import AppRoute from './appRoute';
-import { LoginLayout, MainLayout } from './templates/Layout';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends React.Component {
-
-  componentDidMount() {
-    document.body.classList.add("pace-done");
-  }
-
-  render() {
-    return (
-        <Router>
-          <div className="App gray-bg">
-            <Switch>
-              <AppRoute exact path='/' component={Login} layout ={LoginLayout}/>
-              <AppRoute path='/Dashboard' component={Dashboard} layout ={MainLayout}/>
-              <AppRoute path='/ProfileContact' component={ProfileContact} layout ={MainLayout}/>
-            </Switch>
-          </div>
-        </Router> 
-    );
-  }
-
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
